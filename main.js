9201eb5739271c2cf3c5348dbe9ff84928a1e56f
walter ladislao <walterladislao00@gmail.com>
2024-04-29 19:42:04 +0200

    titolo sezione miei lavori aggiornato

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('myNav');
-    // Verifica se lo scrolling è verso il basso
-    if (window.scrollY > this.lastScroll) {
-        // Nascondi la navbar spostandola verso l'alto
-        navbar.style.top = '-100px'; // Modifica il valore in base all'altezza della tua navbar
+    
+    if (window.scrollY > this.lastScroll) {        
+        navbar.style.top = '-100px';
     } else {
-        // Mostra la navbar spostandola verso il basso
         navbar.style.top = '0';
     }
-    // Salva l'ultima posizione dello scrolling
     this.lastScroll = window.scrollY;
 });
